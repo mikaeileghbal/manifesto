de7c9b369f7e0a85fe876ccc82a3b3bb25826582
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-09-23 11:26:16 +0330

    Fix menu overlay appear animations

--- code/index.js
+++ code/index.js
@@ -26,15 +26,35 @@ function menuMouseLeave(e) {
 }
 
 const menuTimeline = gsap.timeline({});
-const menuAnimation = menuTimeline.to(".nav-menu", { opacity: 1, duration: 2 });
+const menuAnimation = menuTimeline.to(".nav-menu", {
+  opacity: 1,
+  duration: 0.5,
+});
+
+const menuTimelineR = gsap.timeline({});
+// const menuAnimationR = menuTimeline.to(".nav-menu", {
+//   opacity: 0,
+//   duration: 0.5,
+// });
+//menuAnimationR.pause();
+
 const menuItemTimeline = gsap.timeline({});
 const menuItemAnimation = menuItemTimeline.to(".nav-item .menu-first", {
   y: 0,
-  duration: 1,
-  stagger: 0.2,
+  duration: 0.8,
+  delay: 0.5,
+  stagger: 0.1,
 });
 menuAnimation.pause();
 menuItemAnimation.pause();
+
+const menuItemTimelineR = gsap.timeline({});
+const menuItemAnimationR = menuItemTimelineR.to(".nav-item .menu-first", {
+  y: "-140px",
+  duration: 0.8,
+  stagger: 0.1,
+});
+menuItemAnimationR.pause();
 
 menu.addEventListener("click", animateMenuOverlay);
 let showMenu = false;
@@ -45,8 +65,8 @@ function animateMenuOverlay() {
     menuItemAnimation.play();
     showMenu = true;
   } else {
-    menuAnimation.reverse();
-    menuItemAnimation.reverse();
+    menuItemAnimationR.play();
+    //menuAnimationR.play();
     showMenu = false;
   }
 }
