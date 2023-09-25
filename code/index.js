7e3ff382d113a23ec39c3a84d919e393e84d2d63
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-09-25 13:02:54 +0330

    Complete menu overlay appear animation

--- code/index.js
+++ code/index.js
@@ -25,48 +25,75 @@ function menuMouseLeave(e) {
   mouseTail.classList.remove("menu");
 }
 
-const menuTimeline = gsap.timeline({});
-const menuAnimation = menuTimeline.to(".nav-menu", {
-  opacity: 1,
-  duration: 0.5,
+const menuTimeline = gsap.timeline({
+  paused: true,
 });
+const menuAnimation = menuTimeline
+  .to(".nav-menu", {
+    opacity: 1,
+    duration: 0.3,
+  })
+  .to(".nav-item .menu-first", {
+    y: 0,
+    duration: 0.6,
+    ease: "power4.easeOut",
+    //delay: 0.5,
+    stagger: 0.1,
+  });
 
-const menuTimelineR = gsap.timeline({});
+const menuTimelineReverse = gsap.timeline({
+  paused: true,
+});
+const menuAnimationReverse = menuTimelineReverse
+  .to(".nav-item .menu-first", {
+    y: "-100%",
+    duration: 0.5,
+    ease: "power4.easeOut",
+    //delay: 0.5,
+    stagger: 0.06,
+  })
+  .to(".nav-menu", {
+    opacity: 0,
+    duration: 0.3,
+  });
+
+//const menuTimelineR = gsap.timeline({});
 // const menuAnimationR = menuTimeline.to(".nav-menu", {
 //   opacity: 0,
 //   duration: 0.5,
 // });
 //menuAnimationR.pause();
 
-const menuItemTimeline = gsap.timeline({});
-const menuItemAnimation = menuItemTimeline.to(".nav-item .menu-first", {
-  y: 0,
-  duration: 0.8,
-  delay: 0.5,
-  stagger: 0.1,
-});
-menuAnimation.pause();
-menuItemAnimation.pause();
+//const menuItemTimeline = gsap.timeline({});
+// const menuItemAnimation = menuItemTimeline.to(".nav-item .menu-first", {
+//   y: 0,
+//   duration: 0.8,
+//   ease: "power2.out",
+//   delay: 0.5,
+//   stagger: 0.1,
+// });
+//menuAnimation.pause();
+//menuItemAnimation.pause();
 
-const menuItemTimelineR = gsap.timeline({});
-const menuItemAnimationR = menuItemTimelineR.to(".nav-item .menu-first", {
-  y: "-140px",
-  duration: 0.8,
-  stagger: 0.1,
-});
-menuItemAnimationR.pause();
+// const menuItemTimelineR = gsap.timeline({});
+// const menuItemAnimationR = menuItemTimelineR.to(".nav-item .menu-first", {
+//   y: "-140px",
+//   duration: 0.8,
+//   ease: "power2.out",
+//   stagger: 0.1,
+// });
+// menuItemAnimationR.pause();
 
 menu.addEventListener("click", animateMenuOverlay);
+
 let showMenu = false;
+
 function animateMenuOverlay() {
-  console.log("menu clicked");
   if (!showMenu) {
-    menuAnimation.play();
-    menuItemAnimation.play();
+    menuAnimation.play(0);
     showMenu = true;
   } else {
-    menuItemAnimationR.play();
-    //menuAnimationR.play();
+    menuAnimationReverse.play(0);
     showMenu = false;
   }
 }
